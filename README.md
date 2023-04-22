7ae3981549bd8f4f5718b29aa7472c270c82dc7b
Ryan Cloherty <cloherty.ryan@gmail.com>
2023-04-22 13:39:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NorthstarAgonesPlugin
+A Northstar server plugin that integrates with the Agones SDK
---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use rrplug::prelude::*;
+use rrplug::{
+    bindings::convar::FCVAR_GAMEDLL, sq_return_null,
+    wrappers::convars::{ConVarRegister, ConVarStruct},
+    wrappers::northstar::{EngineLoadType, PluginData},
+};
+
+#[derive(Debug)]
+pub struct ExamplePlugin;
+
+impl Plugin for ExamplePlugin {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn initialize(&mut self, plugin_data: &PluginData) {
+        log::info!("yay logging :D");
+
+        _ = plugin_data.register_sq_functions(info_example);
+    }
+
+    fn main(&self) {}
+
+    fn on_engine_load(&self, engine: EngineLoadType) {
+        let engine = match engine {
+            EngineLoadType::Engine(engine) => engine,
+            EngineLoadType::EngineFailed => return,
+            EngineLoadType::Server => return,
+            EngineLoadType::Client => return,
+        };
+
+        let convar = ConVarStruct::try_new().unwrap();
+        let register_info = ConVarRegister {
+            callback: Some(basic_convar_changed_callback),
+            ..ConVarRegister::mandatory(
+                "basic_convar",
+                "48",
+                FCVAR_GAMEDLL.try_into().unwrap(),
+                "basic_convar",
+            )
+        };
+
+        convar.register(register_info).unwrap();
+
+        _ = engine.register_concommand(
+            "basic_command",
+            basic_command_callback,
+            "basic_command",
+            FCVAR_GAMEDLL.try_into().unwrap(),
+        );
+    }
+}
+
+#[rrplug::concommand]
+fn basic_command_callback(command: CCommandResult) {
+    log::info!("running basic_command");
+    log::info!("args: {:?}", command.args)
+}
+
+#[rrplug::convar]
+fn basic_convar_changed_callback(convar: Option<ConVarStruct>, old_value: String, float_old_value: f32) {
+    log::info!("old value: {}", float_old_value)
+}
+
+#[rrplug::sqfunction(VM=Client,ExportName=BasicExample)]
+fn example(name: String) {
+    log::info!("exmaple {name}");
+
+    sq_return_null!()
+}
+
+entry!(ExamplePlugin);
